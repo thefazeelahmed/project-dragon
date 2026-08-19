86da606c62841e47fc1bc4cbc87b3222ccbf6472
Fazeel Ahmed <58031420+thefazeelahmed@users.noreply.github.com>
2026-08-20 00:19:01 +0500

    chore(backendteam-member): add team member core crud

--- team_members/models.py
+++ team_members/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class TeamMember(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
